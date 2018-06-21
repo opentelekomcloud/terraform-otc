5d4c45ea3617c074493cca9e918394ea67bbf5e9
Christian Eichelmann <ceichelmann@gmx.de>
2018-06-21 11:55:01 +0200

    use the new parameter instead of the deprecated one

--- full/router.tf
+++ full/router.tf
@@ -1,7 +1,7 @@
 resource "openstack_networking_router_v2" "router" {
-  name             = "${var.project}-router"
-  admin_state_up   = "true"
-  external_gateway = "${data.openstack_networking_network_v2.extnet.id}"
+  name                = "${var.project}-router"
+  admin_state_up      = "true"
+  external_network_id = "${data.openstack_networking_network_v2.extnet.id}"
 }
 
 resource "openstack_networking_router_interface_v2" "interface" {
